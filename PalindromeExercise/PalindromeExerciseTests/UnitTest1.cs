8d22b1da55ecc6a7265061c1cfc05f55f0a15b45
cpheighter <cheighterjr@gmail.com>
2022-07-25 16:25:36 -0400

    made method and test

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("kayak", true)]
+        [InlineData("bacon", false)]
+        [InlineData("noon", true)]
+        public void Test1(string x, bool expected)
         {
-
+            //Arrange
+            WordSmith wordsmith = new WordSmith();
+            //Act
+            var actual = wordsmith.IsAPalindrome(x);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
