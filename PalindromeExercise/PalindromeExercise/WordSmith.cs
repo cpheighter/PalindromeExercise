3bd3c520a007698342578ee9ca61995f861b628b
cpheighter <cheighterjr@gmail.com>
2022-07-25 16:26:12 -0400

    2nd commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string x)
+        {
+            string reverse = new string (x.Reverse().ToArray());
+            if (reverse == x)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
